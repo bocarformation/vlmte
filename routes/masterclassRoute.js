663e392e0c1bbe87f8688eba1ec56dade5bd2e9c
bocarformation <bocsco3@gmail.com>
2023-08-31 01:12:44 +0200

    update http protocol

--- routes/masterclassRoute.js
+++ routes/masterclassRoute.js
@@ -21,7 +21,7 @@ router.post("/new",uploadS3.fields([{
     name: "video"
 }]),  AddMasterclass)
 
-router.put("/edit/:id",uploadS3.fields([{
+router.post("/edit/:id",uploadS3.fields([{
 
     name: "logo"
 
